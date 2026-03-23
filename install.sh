f72e4272885a8421267d7fa583a289142d0ee5e2
Roberto Rodriguez Jr <64380209+hardkorebob@users.noreply.github.com>
2026-03-22 21:48:43 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ strip -s swm
 mkdir $CONF 
 cp config $CONF
 chmod +x swmctl swm-session
-sudo cp swmctl swm-session swm /usr/local/bin
+sudo cp swmctl swm-session swm $DIR
 sudo cp swm.desktop /usr/share/xsessions
